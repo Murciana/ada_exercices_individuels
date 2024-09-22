72cc5d72ee836bb16f50afe9233be33ed5e4d7a7
Murciana <murciana.lb@gmail.com>
2024-09-22 18:14:42 +0200

    ajout fonction MAJ des fonctions tryColors et checkCombination

--- 08_mastermind/script.js
+++ 08_mastermind/script.js
@@ -5,25 +5,55 @@ Combinaison à deviner : 2 pions de couleurs différentes
 - Une fonction qui retourne true ou false si la bonne combinaison est trouvée ou non
 - Une fonction qui gère la partie (continuer tant que/fin si gagné)
 */
-let codemakerCombination = ["purple", "yellow"]
+let comboTests = 12
+let codemakerCombination = ["purple","yellow"]
+console.log("codeMaker's proposition: ",codemakerCombination);
 
 function tryColors() {
-let colorChoices = ["yellow", "green", "purple", "orange"]
-let gameRules = alert(`Here are the 4 colors possible : ${colorChoices}`)
-let codebreakerInput = prompt("Let's guess codemaker's 2-colors' combinaition")
-let codebreakerProposition = codebreakerInput.split(',')
-
-if (!codebreakerProposition.every((element) =>colorChoices.includes(element))) {
-    codebreakerInput = prompt(`Invalid proposition. Please choose between ${colorChoices}`)
-} 
-    console.log(codebreakerProposition)
+    let colorChoices = ["yellow", "green", "purple", "orange"]
+    let codebreakerInput = prompt(`Let's guess codemaker's 2-colors combinaition. \nHere are the 4 colors possible : ${colorChoices}`)
+    let codebreakerProposition = codebreakerInput.split(',') // on sépare les différentes couleurs de la chaine de caractère
+    
+    // On vérifie que la longueur est exactement 2 et que chaque couleur proposée est dans `colorChoices`
+    while ((codebreakerProposition.length < 2) || (codebreakerProposition.length > 2) || !codebreakerProposition.every((element) =>colorChoices.includes(element))) { 
+        codebreakerInput = prompt(`Invalid proposition. Please choose between ${colorChoices}`)    
+        codebreakerProposition = codebreakerInput.split(','); // mise à jour 
+    } 
+    console.log("codeBreaker's proposition: ",codebreakerProposition)
     return codebreakerProposition
 }
-tryColors()
 
 function checkCombination() {
+    let codebreakerCombination = tryColors()
 
+    if (codemakerCombination.join() === codebreakerCombination.join()){
+        console.log("you won");     
+        return true
+
+    } else {        
+        let wellPlaced = []; 
+        let missPlaced = [];
+        let notInCombo = [];  
+
+        for (let i = 0; i < codebreakerCombination.length; i++) { 
+            if (codemakerCombination[i] == codebreakerCombination[i]) { 
+            wellPlaced.push(codebreakerCombination[i]);       
+            }	else if (!codemakerCombination.includes(codebreakerCombination[i])){
+            notInCombo.push(codebreakerCombination[i])
+            } else {
+                missPlaced.push(codebreakerCombination[i])        
+                }
+            }
+        console.log({ wellPlaced: wellPlaced, missPlaced: missPlaced, notInCombo: notInCombo });
+        
+        comboTests --
+        console.log(comboTests);
+        
+        return { wellPlaced: wellPlaced, missPlaced: missPlaced, notInCombo: notInCombo }
+        }
+        
 }
+checkCombination()
 
 function gamePlay() {
 
